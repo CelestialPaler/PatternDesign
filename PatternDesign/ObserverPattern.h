767efb3ba192ce4f51f3bf740bad49a03b2b6388
天师苍邪 <sun1106153343@hotmail.com>
2018-11-20 11:03:13 +0800

    Adding Observer Pattern and examples.

--- PatternDesign/ObserverPattern.h
+++ PatternDesign/ObserverPattern.h
@@ -7,30 +7,58 @@
 #pragma once
 #include <vector>
 
-class Observable
-{
-public:
-	void Register(Observer & const _ob) {
-		subscribers.push_back(_ob);
-	}
-
-	void Unregister(Observer & const _ob) {
-		subscribers.erase(std::find(subscribers.cbegin(), subscribers.cend(), _ob));
-	}
-
-private:
-	virtual void notify(void) {
-		for (Observer & ob : subscribers)
-			ob.Update();
-	}
-
-	std::vector<Observer & const> subscribers;
-};
-
 class Observer
 {
 public:
-	virtual void Update(void) = 0;
+	virtual void Update(const int _temp) = 0;
+};
+
+class Observable
+{
+public:
+	virtual void Register(Observer * _ob) = 0;
+	virtual void Unregister(Observer * _ob) = 0;
+
 private:
-	virtual void GetState(void) = 0;
+	virtual void Notify(void) = 0;
+};
+
+
+class Phone : public Observer
+{
+public:
+	void Update(const int _temp) override
+	{
+		std::cout << "New data arrived : " << _temp << std::endl;
+	}
+};
+
+class WeatherStation : public Observable
+{
+public:
+	void Register(Observer * _ob) override
+	{
+		subscribers.push_back(_ob);
+	}
+
+	void Unregister(Observer * _ob) override
+	{
+		subscribers.erase(std::find(subscribers.cbegin(), subscribers.cend(), _ob));
+	}
+
+public:
+	void SetTemperature(const int _temp)
+	{
+		this->temperature = _temp;
+		this->Notify();
+	}
+private:
+	void Notify(void) override
+	{
+		for (Observer * ob : subscribers)
+			ob->Update(temperature);
+	}
+private:
+	std::vector<Observer *> subscribers;
+	int temperature;
 };
--- PatternDesign/PatternDesign.cpp
+++ PatternDesign/PatternDesign.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 
 #include "SingletonPattern.h"
+#include "ObserverPattern.h"
 
 void SingletonExample(void)
 {
@@ -17,6 +18,30 @@ void SingletonExample(void)
 	std::cout << "Address of st2 : " << st2 << std::endl;
 }
 
+void ObserverExample(void)
+{
+	std::cout << "A example of observer pattern : \n"
+		<< "Phones that subscribed a weather station for temperature." 
+		<< std::endl;
+	WeatherStation weatherStation;
+	Phone phone1;
+	Phone phone2;
+
+	weatherStation.Register(&phone1);
+	weatherStation.Register(&phone2);
+
+	weatherStation.SetTemperature(28);
+
+	Phone phone3;
+	weatherStation.Register(&phone3);
+
+	weatherStation.SetTemperature(25);
+
+	weatherStation.Unregister(&phone1);
+	weatherStation.Unregister(&phone2);
+
+	weatherStation.SetTemperature(27);
+}
 enum Patterns 
 {
 	SingletonPattern,
@@ -25,15 +50,15 @@ enum Patterns
 
 int main(int argc, char * argv[]) 
 {
-	switch (Patterns()) 
+	switch (Patterns::ObserverPattern) 
 	{
 	case SingletonPattern:
 		SingletonExample();
 		break;
 	case ObserverPattern:
+		ObserverExample();
 		break;
 	default:
 		break;
 	}
 }
-
