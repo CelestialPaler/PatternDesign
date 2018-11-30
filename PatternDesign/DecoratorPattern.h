cf63ee1239e19f7e76f4796e4991bdbe6ae3db29
天师苍邪 <sun1106153343@hotmail.com>
2018-11-30 22:26:30 +0800

    Uploaded some files.

---
+++ PatternDesign/DecoratorPattern.h
@@ -0,0 +1,23 @@
+﻿/***************************************************************************************************/
+/*								        		 			Pattern Design  														      */
+/*								        		 	      Decorator Pattern     														  */
+/*                                                    www.tianshicangxie.com                                                       */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+#pragma once
+
+class Coffee
+{
+public:
+	virtual unsigned int cost(void) = 0;
+};
+
+class Decorator
+{
+public:
+
+private:
+	Decorator * concreateObj;
+};
+
+
